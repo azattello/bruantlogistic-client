2e0dbe26306da4c4e74ea3f21d7ab248ad6ca6a6
Abdykali Azat <azat.abdykalli@gmail.com>
2024-11-06 02:20:08 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,12 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://bruantlogistic.kz:3001',
+   
+    // nameCargo: 'TURKESTAN KARGO'
+    nameCargo: 'Bruant Logistic'
+  };
+  
+  
+export default config;
+  
